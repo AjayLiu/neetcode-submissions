3a95638c23180b807557c9b7693208657a7bfb41
AjayLiu <ajayliu2928@gmail.com>
2026-05-26 12:18:35 +0200

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self):
+        self.children = [None] * 26
+        self.isEnd = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        n = self.root
+        for c in word:
+            idx = ord(c) - ord('a')
+            if not n.children[idx]:
+                n.children[idx] = Node()
+            n = n.children[idx]
+        n.isEnd = True
+
+    def search(self, word: str) -> bool:
+        n = self.root
+        for c in word:
+            idx = ord(c) - ord('a')
+            if not n.children[idx]:
+                return False
+            n = n.children[idx]
+        return n.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        n = self.root
+        for c in prefix:
+            idx = ord(c) - ord('a')
+            if not n.children[idx]:
+                return False
+            n = n.children[idx]
+        return True
+        
+        
